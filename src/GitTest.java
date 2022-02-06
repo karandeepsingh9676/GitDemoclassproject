5d6e9b84740a89fe260c18ad1d55bb3aaf831d34
karandeepsingh9676 <99137384+karandeepsingh9676@users.noreply.github.com>
2022-02-06 23:58:38 +0530

    Update GitTest.java

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,7 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        
         System.out.println("second line ");
         System.out.println("3rd line ");
         
